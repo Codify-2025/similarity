3acff81d002d0ecdc36bfe86e9c7966739465140
dylee00 <dylee6820@gmail.com>
2025-08-02 17:28:19 +0900

    Chore:프로젝트 초기 설정

---
+++ src/test/java/Codify/similarity/SimilarityApplicationTests.java
@@ -0,0 +1,13 @@
+package Codify.similarity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
